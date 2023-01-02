8b137505ed7e5f74f29ae7e36d8224c618388360
Yczar <babalolagbogo@gmail.com>
2023-01-02 04:31:38 +0100

    :construction: chore: "initialize project"

---
+++ flutter/how_to_integrate_google_sign_in_and_oauth_into_your_flutter_apps/android/app/src/main/kotlin/com/example/how_to_integrate_google_sign_in_and_oauth_into_your_flutter_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.how_to_integrate_google_sign_in_and_oauth_into_your_flutter_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
